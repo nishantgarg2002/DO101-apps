f883973cd3146a9af37ecdbb9c330df6f00bbd87
nishantgarg2002 <nishant0884.be20@chitkara.edu.in>
2022-05-13 12:29:28 +0530

    Add

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,7 +4,9 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
-
+app.get('/Nishant', function (req, res) {
+  res.send('Hello Globe!\n');
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
